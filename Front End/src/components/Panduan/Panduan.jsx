130e6f344e787025889af9fd3556672ed0fc1cee
EdenWijaya <eden.122140187@student.itera.ac.id>
2024-11-23 12:44:34 +0700

    Push (FE) : update panduan image

--- Front End/src/components/Panduan/Panduan.jsx
+++ Front End/src/components/Panduan/Panduan.jsx
@@ -4,18 +4,18 @@ import { motion } from "framer-motion";
 const sections = [
   {
     id: 1,
-    title: "Menjaga Lingkungan dari Pembuangan Sampah Sembarangan",
+    title: "Pembuangan Sampah",
     content:
       "Membiasakan diri untuk membuang sampah pada tempatnya. Misalkan ketika kita menemukan sampah yang berserakan, kita bisa membuangnya ke dalam tempat sampah. Sehingga tidak hanya petugas kebersihan saja yang melakukannya, tetapi kita juga bisa ikut menjaga kebersihan lingkungan.",
-    image: "assets/11.png",
+    image: "assets/membuang_sampah.jpg",
     delay: 0.2,
   },
   {
     id: 2,
-    title: "Melakukan Pemilahan Sampah Sesuai Jenisnya",
+    title: "Pemilahan Sampah",
     content:
       "Terdapat dua jenis sampah, yaitu sampah organik dan sampah anorganik. Sampah organik yaitu sampah basah yang bisa diolah dan dijadikan pupuk. Sedangkan sampah anorganik atau sampah kering adalah sampah yang tidak bisa diurai atau membusuk. Pemilahan sampah perlu dilakukan untuk menjaga lingkungan sekitar agar tetap bersih dan indah.",
-    image: "assets/11.png",
+    image: "assets/pemilahan.jpg",
     delay: 0.4,
   },
   {
@@ -23,7 +23,7 @@ const sections = [
     title: "Kegiatan Kerja Bakti",
     content:
       "Menjaga kebersihan lingkungan bisa dilakukan dengan rutin melalui kegiatan kerja bakti atau gotong royong. Hal ini bertujuan untuk menjaga lingkungan agar tetap bersih dan mempererat hubungan antar warga. Sehingga bisa mewujudkan kebersihan lingkungan bersama. Kerja bakti atau gotong royong bisa dilakukan sekali dalam seminggu.",
-    image: "assets/kerja_bakti.jpg",
+    image: "assets/kerja_baktii.jpg",
     delay: 0.6,
   },
   {
@@ -31,7 +31,7 @@ const sections = [
     title: "Kampanye Kesadaran Kesehatan",
     content:
       "Mengadakan kampanye edukasi kesadaran kesehatan agar setiap warga desa mengerti bagaimana cara menjaga tubuh dan lingkungannya agar tetap sehat.",
-    image: "assets/",
+    image: "assets/kesehatan.png",
     delay: 0.8,
   },
   {
@@ -39,15 +39,15 @@ const sections = [
     title: "Penghijauan Lingkungan",
     content:
       "Memperbanyak tanaman hijau di sekitar agar suasana lebih asri dan rindang. Tanaman hijau dapat melepaskan oksigen dan menyerap karbon dioksida. Inilah salah satu alasan bahwa tanaman hijau bisa membuat kualitas udara jadi lebih baik.",
-    image: "assets/penghijauan_lingkungan.jpg",
+    image: "assets/penghijauan.jpg",
     delay: 1.0,
   },
 
   {
     id: 6,
-    title : "lorem ipsum",
-    content: "lorem ipsum",
-    image: "assets/11.png",
+    title : "Pengurangan Penggunaan Plastik",
+    content: "Mengurangi penggunaan plastik sekali pakai adalah langkah kecil dengan dampak besar. Dengan tas belanja kain, botol minum isi ulang, dan sedotan non plastik, kita dapat membantu mengurangi limbah plastik yang mencemari lingkungan. Mulailah dari hal sederhana untuk masa depan yang lebih hijau",
+    image: "assets/plastik.jpg",
     delay: 1.2,
   },
 ];
